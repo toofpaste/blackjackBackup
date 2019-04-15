0259a3fc326c6249f8b73c8686333c26cf75798e
Joe Barnes and Michael Larragueta <michael@>
2019-04-15 14:50:47 -0700

    Add new hand functionality

--- js/scripts.js
+++ js/scripts.js
@@ -25,128 +25,155 @@ function addCard(rngVal){
   };
 };
 
-function checkWin(totalUser, totalDeal){
+function checkWin(totalUser, totalDeal, playerBank, betTotal){
+  $("#reset").show();
   if (totalUser >= 22){
-     console.log("   User: " + totalUser + "    BUST - - - - Dealer Wins");
+     $('#name').text("   User: " + totalUser + "    BUST - - - - Dealer Wins");
+     $('#bet').text("Bet Amount: $" + betTotal + " Loss Amount: $" + (betTotal));
+     $("#bank").text("Bank Balance: $" + playerBank);
    };
    if (totalUser === totalDeal){
-     console.log("Dealer: " + totalDeal + "  /User: " + totalUser + "    TIE TIE TIE TIE TIE");
+     playerBank += betTotal[0];
+     $('#name').text("Dealer: " + totalDeal + "  /User: " + totalUser + "    TIE TIE TIE TIE TIE");
+     $('#bet').text("Bet Amount: $" + betTotal + " Win Amount: $" + (betTotal));
+     $("#bank").text("Bank Balance: $" + playerBank);
    };
    if (totalUser > totalDeal && totalUser <= 21){
-     console.log("Dealer: " + totalDeal + "   /User: " + totalUser + "    USER WINS");
+     playerBank += (betTotal[0]*2);
+    $('#name').text("Dealer: " + totalDeal + "   /User: " + totalUser + "    USER WINS")
+    $('#bet').text("Bet Amount: $" + betTotal + " Win Amount: $" + (betTotal * 2));
+    $("#bank").text("Bank Balance: $" + playerBank); ;
    };
    if (totalDeal > totalUser && totalDeal <= 21){
-     console.log("Dealer: " + totalDeal + "   /User: " + totalUser + "    DEALER WINS HAHAHA");
+    $('#name').text("Dealer: " + totalDeal + "   /User: " + totalUser + "    DEALER WINS HAHAHA");
+    $('#bet').text("Bet Amount: $" + betTotal + " Loss Amount: $" + (betTotal));
+    $("#bank").text("Bank Balance: $" + playerBank);
    };
    if(totalDeal > 21 && totalUser <= 21){
-     console.log("Dealer: " + totalDeal + "   /User: " + totalUser + "    USER WINS:  DEALER BUST");
+     playerBank += (betTotal[0]*2);
+     $('#name').text("Dealer: " + totalDeal + "   /User: " + totalUser + "    USER WINS:  DEALER BUST");
+     $('#bet').text("Bet Amount: $" + betTotal + " Win Amount: $" + (betTotal * 2));
+     $("#bank").text("Bank Balance: $" + playerBank);
    };
 };
+function start(playerBank){
+  $("#reset").hide();
+  var suitName = ["Spades" , "Hearts", "Diamonds", "Clubs"];
+  var valueName = [2, 3, 4, 5, 6, 7 ,8 ,9, 10, "Jack", "Queen", "King", "Ace"];
+  //               0  1  2  3  4  5  6  7  8     9        10      11      12
+  var totalUser = 0;
+  var totalDeal = 0;
+  var rngVal = 0;
+  var rngSuit = 0;
+  var num = 0;
+  var betTotal = [];
 
-function bet(num, betTotal){
-
-
-  $("#printBet").text("NO BET");
-  $("#plus1").click(function(){
+  $("#plus1").unbind("click").click(function(){
     $("button#dealC").show();
-    num=1;
-    betTotal+=1
-    $("#printBet").text("BET: $" + betTotal);
-    // takeMoney(num, betTotal);
+    var clickedNum = 1;
+    playerBank = bet(playerBank, clickedNum, betTotal);
   })
 
-  $("#plus5").click(function(){
+  $("#plus5").unbind("click").click(function(){
     $("button#dealC").show();
-    num=5;
-    betTotal+=5
-    $("#printBet").text("BET: $" + betTotal);
-    // takeMoney(num, betTotal);
+    var clickedNum = 5;
+    playerBank = bet(playerBank, clickedNum, betTotal);
   })
 
-  $("#plus10").click(function(){
+  $("#plus10").unbind("click").click(function(){
     $("button#dealC").show();
-    num=10;
-    betTotal+=10
-    $("#printBet").text("BET: $" + betTotal);
-    // takeMoney(num, betTotal);
-    return betTotal;
+    var clickedNum = 10;
+    playerBank = bet(playerBank, clickedNum, betTotal);
   })
 
-  $("#plus50").click(function(){
+  $("#plus50").unbind("click").click(function(){
     $("button#dealC").show();
-    num=50;
-    betTotal+=50
-    $("#printBet").text("BET: $" + betTotal);
-    // takeMoney(num, betTotal);
+    var clickedNum = 50;
+    playerBank = bet(playerBank, clickedNum, betTotal);
   })
 
-  $("#plus100").click(function(){
+  $("#plus100").unbind("click").click(function(){
     $("button#dealC").show();
-    num=100;
-    betTotal+=100
-    $("#printBet").text("BET: $" + betTotal);
-    // takeMoney(num, betTotal);
+    var clickedNum = 100;
+    playerBank = bet(playerBank, clickedNum, betTotal);
   })
+
+  $("button#dealC").unbind("click").click(function(){
+    $("#plus1").hide();
+    $("#plus5").hide();
+    $("#plus10").hide();
+    $("#plus50").hide();
+    $("#plus100").hide();
+    $("button#dealC").hide();
+    $("button#hit").show();
+    $("button#stay").show();
+      rngVal = randVal();
+      totalUser += addCard(rngVal);
+      rngSuit = randSuit();
+      dealUser(rngVal, rngSuit, suitName, valueName);
+      rngVal = randVal();
+      totalUser += addCard(rngVal);
+      rngSuit = randSuit();
+      dealUser(rngVal, rngSuit, suitName, valueName);
+      rngVal = randVal();
+      totalDeal += addCard(rngVal);
+      rngSuit = randSuit();
+      dealDeal(rngVal, rngSuit, suitName, valueName);
+    });
+    $("button#hit").unbind("click").click(function(){
+      if(totalUser <= 21){
+      rngVal = randVal();
+      totalUser += addCard(rngVal);
+      rngSuit = randSuit();
+      dealUser(rngVal, rngSuit, suitName, valueName);
+      };
+    });
+    $("button#stay").unbind("click").click(function(){
+      $("#stay").hide();
+      $("#hit").hide();
+    while(totalDeal <= 16){
+      rngVal = randVal();
+      totalDeal += addCard(rngVal);
+      rngSuit = randSuit();
+      dealDeal(rngVal, rngSuit, suitName, valueName);
+    };
+    checkWin(totalUser, totalDeal, playerBank, betTotal);
+    });
+};
+
+function bet(balance, clickedNum, betTotal){
+balance -= clickedNum;
+betTotal.push(clickedNum);
+if(betTotal.length != 1){
+betTotal[0] += betTotal.pop();
+};
+$("#printBet").text("BET: $" + betTotal[0]);
+
+
+return balance;
 }
-
+function sum(total, num){
+  return total + num;
+};
 
 $(function(){
-    var suitName = ["Spades" , "Hearts", "Diamonds", "Clubs"];
-    var valueName = [2, 3, 4, 5, 6, 7 ,8 ,9, 10, "Jack", "Queen", "King", "Ace"];
-    //               0  1  2  3  4  5  6  7  8     9        10      11      12
-
-    var playerBank =1000;
-    var totalUser = 0;
-    var totalDeal = 0;
-    var win = 0;
-    var rngVal = 0;
-    var rngSuit = 0;
-    var num = 0;
-    var betTotal = 0;
-    
-    bet(num, betTotal);
-
-
-    $("button#dealC").click(function(){
-      console.log(betTotal)
-      $("#plus1").hide();
-      $("#plus5").hide();
-      $("#plus10").hide();
-      $("#plus50").hide();
-      $("#plus100").hide();
-      $("button#dealC").hide();
-      $("button#hit").show();
-      $("button#stay").show();
-        rngVal = randVal();
-        totalUser += addCard(rngVal);
-        rngSuit = randSuit();
-        dealUser(rngVal, rngSuit, suitName, valueName);
-        rngVal = randVal();
-        totalUser += addCard(rngVal);
-        rngSuit = randSuit();
-        dealUser(rngVal, rngSuit, suitName, valueName);
-        rngVal = randVal();
-        totalDeal += addCard(rngVal);
-        rngSuit = randSuit();
-        dealDeal(rngVal, rngSuit, suitName, valueName);
+  var playerBank = 1000;
+      $("#reset").unbind("click").click(function(){
+        $("#imgButtons").show();
+        console.log("bank: " + playerBank);
+        $("#plus1").show();
+        $("#plus5").show();
+        $("#plus10").show();
+        $("#plus50").show();
+        $("#plus100").show();
+        $("ul").empty();
+        $("ul").empty();
+        $("#bet").empty();
+        $("#name").empty();
+        $("#bank").empty();
+        $("#hit").hide();
+        $("#stay").hide();
+        $("#printBet").empty();
+        start(playerBank);
       });
-      $("button#hit").click(function(){
-        if(totalUser <= 21){
-        rngVal = randVal();
-        totalUser += addCard(rngVal);
-        rngSuit = randSuit();
-        dealUser(rngVal, rngSuit, suitName, valueName);
-        };
-      });
-      $("button#stay").click(function(){
-      while(totalDeal <= 16){
-        rngVal = randVal();
-        totalDeal += addCard(rngVal);
-        rngSuit = randSuit();
-        dealDeal(rngVal, rngSuit, suitName, valueName);
-      };
-      checkWin(totalUser, totalDeal);
-      });
-
-
 });
