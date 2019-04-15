98334ec267ac2f6b965932372ec261c902f4065b
Joe Barnes and Michael Larragueta <michael@>
2019-04-15 10:01:44 -0700

    Seperate buss logic and user

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,99 @@
+function deal(suit, values, userCard, dealCard){
+  var x = 0;
+  while(x < 2){
+    var rngVal = Math.floor(Math.random() * 13);
+    var rngSuit = Math.floor(Math.random() * 4);
+    if (rngVal > 8 && rngVal < 12){
+      userCard.push(10);
+    }else if (rngVal === 12){
+      userCard.push(11);
+    }else if (rngVal <= 8){
+      userCard.push(rngVal + 2);
+    };
+    $("ul." + suit[0]).prepend("<li>"+ values[rngVal] + " of " + suit[rngSuit] + "</li>")
+    x += 1;
+  };
+    var rngVal = Math.floor(Math.random() * 13);
+    var rngSuit = Math.floor(Math.random() * 4);
+    if (rngVal > 8 && rngVal < 12){
+      dealCard.push(10);
+    }else if (rngVal === 12){
+      dealCard.push(11);
+    }else if (rngVal <= 8){
+      dealCard.push(rngVal + 2);
+    };
+    $("ul." + suit[1]).prepend("<li>"+ values[rngVal] + " of " + suit[rngSuit] + "</li>");
+};
+
+function hitUser(suit, values, userCard, totalUser, dealCard, totalDeal){
+  console.log("hitUser" + totalUser);
+  console.log("hitUser" + totalDeal);
+  var rngVal = Math.floor(Math.random() * 13);
+  var rngSuit = Math.floor(Math.random() * 4);
+  if (rngVal > 8 && rngVal < 12){
+    userCard.push(10);
+  }else if (rngVal === 12){
+    userCard.push(11);
+  }else if (rngVal <= 8){
+    userCard.push(rngVal + 2);
+  };
+  totalUser += userCard[(userCard.length - 1)];
+  $("ul." + suit[0]).prepend("<li>"+ values[rngVal] + " of " + suit[rngSuit] + "</li>");
+  for(var u = 0; u < userCard.length; u += 1){
+  totalUser += userCard[u];
+  };
+  for(var d = 0; d < dealCard.length; d += 1){
+  totalDeal += dealCard[d];
+  };
+};
+
+function stayDeal(suit, values, dealCard, totalDeal){
+  var rngVal = Math.floor(Math.random() * 13);
+  var rngSuit = Math.floor(Math.random() * 4);
+  if (rngVal > 8 && rngVal < 12){
+    dealCard.push(10);
+  }else if (rngVal === 12){
+    dealCard.push(11);
+  }else if (rngVal <= 8){
+    dealCard.push(rngVal + 2);
+  };
+
+   while(totalDeal < 17){
+     var rngVal = Math.floor(Math.random() * 13);
+     var rngSuit = Math.floor(Math.random() * 4);
+     if (rngVal > 8 && rngVal < 12){
+       dealCard.push(10);
+     }else if (rngVal === 12){
+       dealCard.push(11);
+     }else if (rngVal <= 8){
+       dealCard.push(rngVal + 2);
+     };
+     $("ul." + suit[1]).prepend("<li>"+ values[rngVal] + " of " + suit[rngSuit] + "</li>");
+     totalDeal += dealCard[(dealCard.length - 1)];
+   };
+};
+
+function checkWin(win, totalUser, totalDeal){
+  console.log("checkwin" + totalUser);
+  console.log("checkwin" + totalDeal);
+  if(totalDeal >= 17){
+   if (totalUser >= 22){
+     alert("   User: " + totalUser + "    BUST - - - - Dealer Wins")
+   };
+   if (totalUser === totalDeal){
+     alert("Dealer: " + totalDeal + "  /User: " + totalUser + "    TIE TIE TIE TIE TIE")
+   };
+   if (totalUser > totalDeal && totalUser <= 21){
+     alert("Dealer: " + totalDeal + "   /User: " + totalUser + "    USER WINS")
+   };
+   if (totalDeal > totalUser && totalDeal <= 21){
+     alert("Dealer: " + totalDeal + "   /User: " + totalUser + "    DEALER WINS HAHAHA");
+   };
+   if(totalDeal > 21 && totalUser <= 21){
+     alert("Dealer: " + totalDeal + "   /User: " + totalUser + "    USER WINS:  DEALER BUST");
+   };
+};
+};
 
 $(function(){
     var suit = ["Spades" , "Hearts", "Diamonds", "Clubs"];
@@ -9,92 +105,26 @@ $(function(){
     var totalDeal = 0;
     var win = 0;
       $("button#spades").click(function(){
-            var x = 0;
-            while(x < 2){
-              var rngVal = Math.floor(Math.random() * 13);
-              var rngSuit = Math.floor(Math.random() * 4);
-              if (rngVal > 8 && rngVal < 12){
-                userCard.push(10);
-              }else if (rngVal === 12){
-                userCard.push(11);
-              }else if (rngVal <= 8){
-                userCard.push(rngVal + 2);
-              };
-              $("ul." + suit[0]).prepend("<li>"+ values[rngVal] + " of " + suit[rngSuit] + "</li>")
-              x += 1;
-            };
-              var rngVal = Math.floor(Math.random() * 13);
-              var rngSuit = Math.floor(Math.random() * 4);
-              if (rngVal > 8 && rngVal < 12){
-                dealCard.push(10);
-              }else if (rngVal === 12){
-                dealCard.push(11);
-              }else if (rngVal <= 8){
-                dealCard.push(rngVal + 2);
-              };
-              $("ul." + suit[1]).prepend("<li>"+ values[rngVal] + " of " + suit[rngSuit] + "</li>");
-              $("button#hit").click(function(){
-                  var rngVal = Math.floor(Math.random() * 13);
-                  var rngSuit = Math.floor(Math.random() * 4);
-                  if (rngVal > 8 && rngVal < 12){
-                    userCard.push(10);
-                  }else if (rngVal === 12){
-                    userCard.push(11);
-                  }else if (rngVal <= 8){
-                    userCard.push(rngVal + 2);
-                  };
-                  totalUser += userCard[(userCard.length - 1)];
-                  $("ul." + suit[0]).prepend("<li>"+ values[rngVal] + " of " + suit[rngSuit] + "</li>");
-              });
-              for(var u = 0; u < userCard.length; u += 1){
-                totalUser += userCard[u];
-              };
-              for(var d = 0; d < dealCard.length; d += 1){
-                totalDeal += dealCard[d];
-              };
+          deal(suit, values, userCard, dealCard);
+          console.log(userCard);
+          console.log(dealCard);
+          console.log(totalUser);
+          console.log(totalDeal);
+        });
+        $("button#hit").click(function(){
+          hitUser(suit, values, userCard, totalUser, dealCard, totalDeal);
+          console.log(userCard);
+          console.log(dealCard);
+          console.log(totalUser);
+          console.log(totalDeal);
+        });
+        $("button#stay").click(function(){
+          stayDeal(suit, values, dealCard, totalDeal);
+          checkWin(win, totalUser, totalDeal)
+          console.log(userCard);
+          console.log(dealCard);
+          console.log(totalUser);
+          console.log(totalDeal);
+        });
 
-              $("button#stay").click(function(){
-                  var rngVal = Math.floor(Math.random() * 13);
-                  var rngSuit = Math.floor(Math.random() * 4);
-                  if (rngVal > 8 && rngVal < 12){
-                    dealCard.push(10);
-                  }else if (rngVal === 12){
-                    dealCard.push(11);
-                  }else if (rngVal <= 8){
-                    dealCard.push(rngVal + 2);
-                  };
-
-                   while(totalDeal < 17){
-                     var rngVal = Math.floor(Math.random() * 13);
-                     var rngSuit = Math.floor(Math.random() * 4);
-                     if (rngVal > 8 && rngVal < 12){
-                       dealCard.push(10);
-                     }else if (rngVal === 12){
-                       dealCard.push(11);
-                     }else if (rngVal <= 8){
-                       dealCard.push(rngVal + 2);
-                     };
-                     $("ul." + suit[1]).prepend("<li>"+ values[rngVal] + " of " + suit[rngSuit] + "</li>");
-                     totalDeal += dealCard[(dealCard.length - 1)];
-                   };
-
-                  if(totalDeal >= 17){
-                   if (totalUser >= 22){
-                     alert("   User: " + totalUser + "    BUST - - - - Dealer Wins")
-                   };
-                   if (totalUser === totalDeal){
-                     alert("Dealer: " + totalDeal + "  /User: " + totalUser + "    TIE TIE TIE TIE TIE")
-                   };
-                   if (totalUser > totalDeal && totalUser <= 21){
-                     alert("Dealer: " + totalDeal + "   /User: " + totalUser + "    USER WINS")
-                   };
-                   if (totalDeal > totalUser && totalDeal <= 21){
-                     alert("Dealer: " + totalDeal + "   /User: " + totalUser + "    DEALER WINS HAHAHA");
-                   };
-                   if(totalDeal > 21 && totalUser <= 21){
-                     alert("Dealer: " + totalDeal + "   /User: " + totalUser + "    USER WINS:  DEALER BUST");
-                   };
-                 };
-              });
-          });
 });
